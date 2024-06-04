ed542256fc844c0b6fd16d99250e8ac9b906622b
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2024-06-04 16:21:25 +0200

    made Navigation view

---
+++ expo24/src/components/Navigation.jsx
@@ -0,0 +1,31 @@
+import { Outlet, Link } from "react-router-dom";
+
+function Navigation() {
+	return (
+		<>
+			<nav>
+				<div>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/Activities">Activiteiten</Link>
+						</li>
+						<li>
+							<Link to="/Timetable">Timetable</Link>
+						</li>
+						<li>
+							<Link to="/Accessibility">Bereikbaarheid</Link>
+						</li>
+						<li>
+							<Link to="/Practical">Praktische info</Link>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</>
+	);
+}
+
+export default Navigation;

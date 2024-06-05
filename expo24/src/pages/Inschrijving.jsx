16fe24126e3af766264a551994750a2363be7d52
Youssef <youssef.auassar@student.ehb.be>
2024-06-05 15:47:40 +0200

    Background changement

--- expo24/src/pages/Inschrijving.jsx
+++ expo24/src/pages/Inschrijving.jsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 import "../styles/Inschrijving.css";
 
 function Inschrijving() {
-  const [formData, setFormData] = useState({
-    lastName: "",
+	const [formData, setFormData] = useState({
+		lastName: "",
 		firstName: "",
 		email: "",
 		numberOfPeople: "",
@@ -16,9 +16,9 @@ function Inschrijving() {
 			demo3DPrints: false,
 			demoLasercut: false,
 		},
-  })
+	});
 
-  const handleChange = (e) => {
+	const handleChange = (e) => {
 		const { name, value, type, checked } = e.target;
 		if (type === "checkbox") {
 			setFormData((prevState) => ({
@@ -36,11 +36,9 @@ function Inschrijving() {
 		}
 	};
 
-
-
-  const handleSubmit = async (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault();
-    console.log(formData);
+		console.log(formData);
 		const response = await fetch("http://localhost:3000/postInvite", {
 			method: "POST",
 			headers: {
@@ -58,94 +56,145 @@ function Inschrijving() {
 	};
 
 	return (
-		<div className="form-container">
-			<h2 className="form-title">Inschrijving</h2>
-			<form onSubmit={handleSubmit}>
-				<div className="name-forname">
-					<div className="form-group">
-						<label htmlFor="name">Naam:</label>
-						<input type="text" id="name" name="lastName" value={formData.lastName} onChange={handleChange}/>
+		<div className="container">
+			<div className="glass-panel">
+				<div className="glass"></div>
+				<div className="glass"></div>
+				<div className="glass"></div>
+				<div className="glass"></div>
+				<div className="glass"></div>
+				<div className="glass"></div>
+				<div className="glass"></div>
+				<div className="glass"></div>
+				<div className="glass"></div>
+				<div className="glass"></div>
+			</div>
+			<div className="form-container">
+				<h2 className="form-title">Inschrijving</h2>
+				<form onSubmit={handleSubmit}>
+					<div className="name-forname">
+						<div className="form-group">
+							<label htmlFor="name">Naam:</label>
+							<input
+								type="text"
+								id="name"
+								name="lastName"
+								value={formData.lastName}
+								onChange={handleChange}
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="firstName">Voornaam:</label>
+							<input
+								type="text"
+								id="firstName"
+								name="firstName"
+								value={formData.firstName}
+								onChange={handleChange}
+							/>
+						</div>
 					</div>
-					<div className="form-group">
-						<label htmlFor="firstName">Voornaam:</label>
-						<input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange} />
-					</div>
-				</div>
 
-				<div className="email-numberofpeople">
-					<div className="form-group">
-						<label htmlFor="email">Email:</label>
-						<input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
+					<div className="email-numberofpeople">
+						<div className="form-group">
+							<label htmlFor="email">Email:</label>
+							<input
+								type="email"
+								id="email"
+								name="email"
+								value={formData.email}
+								onChange={handleChange}
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="numberOfPeople">Personen:</label>
+							<input type="number" id="numberOfPeople" name="numberOfPeople" />
+						</div>
 					</div>
-					<div className="form-group">
-						<label htmlFor="numberOfPeople">Personen:</label>
-						<input type="number" id="numberOfPeople" name="numberOfPeople" />
-					</div>
-				</div>
 
-				<div className="form-group">
-					<label htmlFor="occupation">Functie:</label>
-					<select id="occupation" name="occupation" value={formData.occupation} onChange={handleChange}>
-						<option value="">Selecteer...</option>
-						<option value="student">Student</option>
-						<option value="docent">Docent</option>
-						<option value="bedrijf">Bedrijf</option>
-						<option value="ouder">Ouder</option>
-						<option value="alumni">Alumni</option>
-					</select>
-				</div>
-				<div className="form-group">
-					<label>Voorkeur Project:</label>
-					<br />
-					<label htmlFor="vrGame">
-						<input type="checkbox" id="vrGame" name="vrGame" value="VR game" checked={formData.preferences.vrGame} onChange={handleChange} />
-						VR-game
-					</label>
-					<br />
-					<label htmlFor="3dGame">
-						<input type="checkbox" id="3dGame" name="threeDGame" value="3D game" checked={formData.preferences.threeDGame} onChange={handleChange}/>
-						3D-game
-					</label>
-					<br />
-					<label htmlFor="liveCoding">
-						<input
-							type="checkbox"
-							id="liveCoding"
-							name="liveCoding"
-							value="Live coding"
-              checked={formData.preferences.liveCoding} 
-              onChange={handleChange}
-						/>
-						Live coderen
-					</label>
-					<br />
-					<label htmlFor="demo3DPrints">
-						<input
-							type="checkbox"
-							id="demo3DPrints"
-							name="demo3DPrints"
-							value="Demo 3D prints"
-              checked={formData.preferences.demo3DPrints} 
-              onChange={handleChange}
-						/>
-						Demo 3D-prints
-					</label>
-					<br />
-					<label htmlFor="demoLasercut">
-						<input
-							type="checkbox"
-							id="demoLasercut"
-							name="demoLasercut"
-							value="Demo lasercut"
-              checked={formData.preferences.demoLasercut} 
-              onChange={handleChange}
-						/>
-						Demo lasersnijden
-					</label>
-					<br />
-				</div>
-				<button type="submit">Verzenden</button>
-			</form>
+					<div className="form-group">
+						<label htmlFor="occupation">Functie:</label>
+						<select
+							id="occupation"
+							name="occupation"
+							value={formData.occupation}
+							onChange={handleChange}
+						>
+							<option value="">Selecteer...</option>
+							<option value="student">Student</option>
+							<option value="docent">Docent</option>
+							<option value="bedrijf">Bedrijf</option>
+							<option value="ouder">Ouder</option>
+							<option value="alumni">Alumni</option>
+						</select>
+					</div>
+					<div className="form-group">
+						<label>Voorkeur Project:</label>
+						<br />
+						<label htmlFor="vrGame">
+							<input
+								type="checkbox"
+								id="vrGame"
+								name="vrGame"
+								value="VR game"
+								checked={formData.preferences.vrGame}
+								onChange={handleChange}
+							/>
+							VR-game
+						</label>
+						<br />
+						<label htmlFor="3dGame">
+							<input
+								type="checkbox"
+								id="3dGame"
+								name="threeDGame"
+								value="3D game"
+								checked={formData.preferences.threeDGame}
+								onChange={handleChange}
+							/>
+							3D-game
+						</label>
+						<br />
+						<label htmlFor="liveCoding">
+							<input
+								type="checkbox"
+								id="liveCoding"
+								name="liveCoding"
+								value="Live coding"
+								checked={formData.preferences.liveCoding}
+								onChange={handleChange}
+							/>
+							Live coderen
+						</label>
+						<br />
+						<label htmlFor="demo3DPrints">
+							<input
+								type="checkbox"
+								id="demo3DPrints"
+								name="demo3DPrints"
+								value="Demo 3D prints"
+								checked={formData.preferences.demo3DPrints}
+								onChange={handleChange}
+							/>
+							Demo 3D-prints
+						</label>
+						<br />
+						<label htmlFor="demoLasercut">
+							<input
+								type="checkbox"
+								id="demoLasercut"
+								name="demoLasercut"
+								value="Demo lasercut"
+								checked={formData.preferences.demoLasercut}
+								onChange={handleChange}
+							/>
+							Demo lasersnijden
+						</label>
+						<br />
+					</div>
+					<button type="submit">Verzenden</button>
+				</form>
+			</div>
 		</div>
 	);
 }

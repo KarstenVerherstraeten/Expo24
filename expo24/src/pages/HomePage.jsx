f36025ff7cb1cdd153110fc23bf29ca8c70040b6
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-06-05 11:10:04 +0200

    Homepage glassmorphis effect added

--- expo24/src/pages/HomePage.jsx
+++ expo24/src/pages/HomePage.jsx
@@ -2,14 +2,27 @@ import Timer from "../components/Timer";
 
 
 function HomePage() {
-	return (
-		<div>
-			<h2>Navigation</h2>
-			<p>Home</p>
-			<Timer></Timer>
-			
-		</div>
-	);
+  return (
+    <div className="container">
+      <div className="glass-panel">
+        <div className="glass"></div>
+        <div className="glass"></div>
+        <div className="glass"></div>
+        <div className="glass"></div>
+        <div className="glass"></div>
+        <div className="glass"></div>
+        <div className="glass"></div>
+        <div className="glass"></div>
+        <div className="glass"></div>
+        <div className="glass"></div>
+      </div>
+      <div className="content">
+        <h2>Navigation</h2>
+        <p>Home</p>
+        <Timer></Timer>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;

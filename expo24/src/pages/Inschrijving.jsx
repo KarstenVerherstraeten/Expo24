d07b8133f6103f86e17f23e4e2968ae3119a5851
Youssef <youssef.auassar@student.ehb.be>
2024-06-05 11:41:31 +0200

    Inschrijving form (aangepast)

--- expo24/src/pages/Inschrijving.jsx
+++ expo24/src/pages/Inschrijving.jsx
@@ -3,50 +3,75 @@ import "../styles/Inschrijving.css"
 
 function Inschrijving() {
     return (
-      <div className="form-container">
-        <h2 className="form-title">Inschrijving</h2>
-        <form>
-          <div className="form-group">
-            <label htmlFor="naam">Naam:</label>
-            <input type="text" id="naam" name="naam" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="voornaam">Voornaam:</label>
-            <input type="text" id="voornaam" name="voornaam" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="aantalPersonen">Aantal Personen:</label>
-            <input type="number" id="aantalPersonen" name="aantalPersonen" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="functie">Functie:</label>
-            <select id="functie" name="functie">
-              <option value="">Selecteer...</option>
-              <option value="student">Student</option>
-              <option value="docent">Docent</option>
-              <option value="bedrijf">Bedrijf</option>
-              <option value="ouder">Ouder</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label htmlFor="voorkeurProject">Voorkeur Project:</label>
-            <select id="voorkeurProject" name="voorkeurProject">
-              <option value="">Selecteer...</option>
-              <option value="VR game">VR game</option>
-              <option value="3D game">3D game</option>
-              <option value="Live coding">Live coding</option>
-              <option value="Demo 3D prints">Demo 3D prints</option>
-              <option value="Demo lasercut">Demo lasercut</option>
-            </select>
-          </div>
-          <button type="submit">Verzenden</button>
-        </form>
-      </div>
+        <div className="form-container">
+            <h2 className="form-title">Inschrijving EXPO.24</h2>
+            <form>
+
+                <div className="name-forname">
+
+                <div className="form-group">
+                    <label htmlFor="name">Naam:</label>
+                    <input type="text" id="name" name="name" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="firstName">Voornaam:</label>
+                    <input type="text" id="firstName" name="firstName" />
+                </div>
+
+                </div>
+
+                <div className="email-numberofpeople">
+
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="numberOfPeople">Aantal Personen:</label>
+                    <input type="number" id="numberOfPeople" name="numberOfPeople" />
+                </div>
+
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="occupation">Functie:</label>
+                    <select id="occupation" name="occupation">
+                        <option value="">Selecteer...</option>
+                        <option value="student">Student</option>
+                        <option value="docent">Docent</option>
+                        <option value="bedrijf">Bedrijf</option>
+                        <option value="ouder">Ouder</option>
+                        <option value="alumni">Alumni</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label>Voorkeur Project:</label><br />
+                    <label htmlFor="vrGame">
+                        <input type="checkbox" id="vrGame" name="vrGame" value="VR game" />
+                        VR-game
+                    </label><br />
+                    <label htmlFor="3dGame">
+                        <input type="checkbox" id="3dGame" name="3dGame" value="3D game" />
+                        3D-game
+                    </label><br />
+                    <label htmlFor="liveCoding">
+                        <input type="checkbox" id="liveCoding" name="liveCoding" value="Live coding" />
+                        Live coderen
+                    </label><br />
+                    <label htmlFor="demo3DPrints">
+                        <input type="checkbox" id="demo3DPrints" name="demo3DPrints" value="Demo 3D prints" />
+                        Demo 3D-prints
+                    </label><br />
+                    <label htmlFor="demoLasercut">
+                        <input type="checkbox" id="demoLasercut" name="demoLasercut" value="Demo lasercut" />
+                        Demo lasersnijden
+                    </label><br />
+                </div>
+                <button type="submit">Verzenden</button>
+            </form>
+        </div>
     );
 }
 
 export default Inschrijving;
+

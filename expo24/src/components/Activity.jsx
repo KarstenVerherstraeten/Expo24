15e1265759b446c559a9e67c75b9e5c26815002b
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2024-06-06 09:55:46 +0200

    made basic component activity WIP

---
+++ expo24/src/components/Activity.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+function Activity() {
+	return (
+		<div>
+			<img src="#" alt="" />
+			<h1>Naam</h1>
+			<h2>Beschrijving</h2>
+		</div>
+	);
+}
+export default Activity;

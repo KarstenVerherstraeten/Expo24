f430b12a511f165bcc375c70576a6d1795c0892e
remygist <remy.gistelinck@student.ehb.be>
2024-06-06 10:09:56 +0200

    add safety check

--- expo24-backend/server.js
+++ expo24-backend/server.js
@@ -7,6 +7,11 @@ require('dotenv').config()
 const port = 3000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGODB_URI
+
+if (!uri) {
+    throw new Error('MONGODB_URI environment variable is not defined');
+  }
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.json());

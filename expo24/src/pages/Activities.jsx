f003a3f4b5fcff4386c0e1f7e56baacb246be119
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-06-06 14:27:27 +0200

    Refactor Activities page layout and styles

--- expo24/src/pages/Activities.jsx
+++ expo24/src/pages/Activities.jsx
@@ -3,21 +3,23 @@ import "../styles/Activities.css";
 
 function Activities() {
 	return (
-		<div>
-			<div className="glass-panel">
-				<div className="glass"></div>
-				<div className="glass"></div>
-				<div className="glass"></div>
-				<div className="glass"></div>
-				<div className="glass"></div>
-				<div className="glass"></div>
-				<div className="glass"></div>
-				<div className="glass"></div>
-				<div className="glass"></div>
-				<div className="glass"></div>
+		<div className="container-3">
+			<div className="glass-panel-3">
+				<div className="glass-3"></div>
+				<div className="glass-3"></div>
+				<div className="glass-3"></div>
+				<div className="glass-3"></div>
+				<div className="glass-3"></div>
+				<div className="glass-3"></div>
+				<div className="glass-3"></div>
+				<div className="glass-3"></div>
+				<div className="glass-3"></div>
+				<div className="glass-3"></div>
 			</div>
 			<div className="ActivitiesContent">
 				<Activity />
+				<Activity />
+				<Activity /> {/* Assuming you have three activities */}
 			</div>
 		</div>
 	);

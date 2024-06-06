e12af0039165ada9be6a730cf245721527d730b2
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2024-06-06 14:29:23 +0200

    Changing styling home  + Confirmation page styling done

--- expo24/src/pages/Confirmation.jsx
+++ expo24/src/pages/Confirmation.jsx
@@ -3,71 +3,73 @@ import { useLocation, useNavigate } from "react-router-dom";
 import "../styles/Confirmation.css";
 
 function Confirmation() {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const { formData } = location.state || {};
+	const location = useLocation();
+	const navigate = useNavigate();
+	const { formData } = location.state || {};
 
-  return (
-    <div>
-      <div className="glass-panel">
-        <div className="glass"></div>
-        <div className="glass"></div>
-        <div className="glass"></div>
-        <div className="glass"></div>
-        <div className="glass"></div>
-        <div className="glass"></div>
-        <div className="glass"></div>
-        <div className="glass"></div>
-        <div className="glass"></div>
-        <div className="glass"></div>
-      </div>
-      <div className="ConfigContent">
-        <h2 className="ConfigTitle">
-          Bedankt voor uw <br /> inschrijving!{" "}
-        </h2>
-        <div className="ConfigInfo">
-          <div>
-            <p>Activiteit</p>
-            <ul>
-              <li>Expo 24</li>
-              <li>Erasmushogeschool Brussel</li>
-              <li>Campus Kaai</li>
-              <li>
-                Nijverheidskaai 170, <br />
-                1070 Brussel
-              </li>
-            </ul>
-          </div>
-          <div>
-            <p>Persoongegevens</p>
-            <ul>
-              <li>{formData?.lastName} {formData?.firstName}</li>
-              <li>{formData?.email}</li>
-              <li>{formData?.numberOfPeople}</li>
-              <li>
-                Voorkeursprojecten
-                <ul>
-                  {formData?.preferences?.vrGame && <li>VR-game</li>}
-                  {formData?.preferences?.threeDGame && <li>3D-game</li>}
-                  {formData?.preferences?.liveCoding && <li>Live coderen</li>}
-                  {formData?.preferences?.demo3DPrints && <li>Demo 3D-prints</li>}
-                  {formData?.preferences?.demoLasercut && <li>Demo lasersnijden</li>}
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="ConfigBottom">
-          <p>
-            U werd succesvol ingeschreven voor EXPO24. <br />U zou binnenkort een email moeten krijgen met alle praktische info.
-          </p>
-        </div>
-        <div className="ConfigButton">
-          <button onClick={() => navigate("/")}>Ga terug naar homepage</button>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="glass-panel">
+				<div className="glass"></div>
+				<div className="glass"></div>
+				<div className="glass"></div>
+				<div className="glass"></div>
+				<div className="glass"></div>
+				<div className="glass"></div>
+				<div className="glass"></div>
+				<div className="glass"></div>
+				<div className="glass"></div>
+				<div className="glass"></div>
+			</div>
+			<div className="ConfigContent">
+				<h2 className="ConfigTitle">
+					Bedankt voor uw <br /> inschrijving!{" "}
+				</h2>
+				<div className="ConfigBottom">
+					<p>
+						U werd succesvol ingeschreven voor EXPO24. <br />U zou binnenkort een email moeten krijgen met alle praktische info.
+					</p>
+				</div>
+				<div className="ConfigInfo">
+					<div>
+						<p>Praktische info:</p>
+						<ul>
+							<p>EXPO24</p>
+							<p>Erasmushogeschool Brussel</p>
+							<p>Campus Kaai</p>
+							<p>
+								Nijverheidskaai 170, <br />
+								1070 Brussel
+							</p>
+						</ul>
+					</div>
+					<div>
+						<p>Persoonlijke gegevens:</p>
+						<ul>
+							<p>
+								{formData?.lastName} {formData?.firstName}
+							</p>
+							<p>{formData?.email}</p>
+							<p>{formData?.numberOfPeople}</p>
+							<p>
+								Voorkeursprojecten
+								<ul>
+									{formData?.preferences?.vrGame && <li>VR-game</li>}
+									{formData?.preferences?.threeDGame && <li>3D-game</li>}
+									{formData?.preferences?.liveCoding && <li>Live coderen</li>}
+									{formData?.preferences?.demo3DPrints && <li>Demo 3D-prints</li>}
+									{formData?.preferences?.demoLasercut && <li>Demo lasersnijden</li>}
+								</ul>
+							</p>
+						</ul>
+					</div>
+				</div>
+				<div className="ConfigButton">
+					<button onClick={() => navigate("/")}>Ga terug naar homepage</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Confirmation;

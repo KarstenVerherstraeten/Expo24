feb6bfea7350b082586ca6a02827292c30d27710
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2024-06-10 14:39:44 +0200

    Made footer + basic styling

--- expo24/src/pages/Activities.jsx
+++ expo24/src/pages/Activities.jsx
@@ -1,27 +1,31 @@
 import Activity from "../components/Activity";
 import "../styles/Activities.css";
+import Footer from "../components/Footer.jsx";
 
 function Activities() {
 	return (
-		<div className="container-3">
-			<div className="glass-panel-3">
-				<div className="glass-3"></div>
-				<div className="glass-3"></div>
-				<div className="glass-3"></div>
-				<div className="glass-3"></div>
-				<div className="glass-3"></div>
-				<div className="glass-3"></div>
-				<div className="glass-3"></div>
-				<div className="glass-3"></div>
-				<div className="glass-3"></div>
-				<div className="glass-3"></div>
+		<>
+			<div className="container-3">
+				<div className="glass-panel-3">
+					<div className="glass-3"></div>
+					<div className="glass-3"></div>
+					<div className="glass-3"></div>
+					<div className="glass-3"></div>
+					<div className="glass-3"></div>
+					<div className="glass-3"></div>
+					<div className="glass-3"></div>
+					<div className="glass-3"></div>
+					<div className="glass-3"></div>
+					<div className="glass-3"></div>
+				</div>
+				<div className="ActivitiesContent">
+					<Activity />
+					<Activity />
+					<Activity /> {/* Assuming you have three activities */}
+				</div>
 			</div>
-			<div className="ActivitiesContent">
-				<Activity />
-				<Activity />
-				<Activity /> {/* Assuming you have three activities */}
-			</div>
-		</div>
+			<Footer></Footer>
+		</>
 	);
 }
 

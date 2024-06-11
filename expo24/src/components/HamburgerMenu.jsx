aef56657b4da4451bb2e1b3df52a46b0cb5dc96d
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2024-06-11 16:10:32 +0200

    WIP hamburgermenu

--- expo24/src/components/HamburgerMenu.jsx
+++ expo24/src/components/HamburgerMenu.jsx
@@ -1,31 +1,31 @@
-import React, { useState } from 'react';
-import { Outlet, Link } from 'react-router-dom';
-import '../styles/HamburgerMenu.css';
+import React, { useState } from "react";
+import { Outlet, Link } from "react-router-dom";
+import "../styles/HamburgerMenu.css";
 
 const HamburgerMenu = () => {
-  const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
 
-  return (
-    <div className={`hamburger-menu ${isOpen ? 'open' : ''}`}>
-      <div className="hamburger-icon" onClick={toggleMenu}>
-        <div className="line"></div>
-        <div className="line"></div>
-        <div className="line"></div>
-      </div>
-      <div className={`menu-items ${isOpen ? 'open' : ''}`}>
-        <Link to="/">Home</Link>
-        <Link to="/Activities">Activiteiten</Link>
-        <Link to="/Timetable">Timetable</Link>
-        <Link to="/Accessibility">Bereikbaarheid</Link>
-        <Link to="/Practical">Praktische info</Link>
-        <Outlet />
-      </div>
-    </div>
-  );
+	return (
+		<div className={`hamburger-menu ${isOpen ? "open" : ""}`}>
+			<div className="hamburger-icon" onClick={toggleMenu}>
+				<div className="line"></div>
+				<div className="line"></div>
+				<div className="line"></div>
+			</div>
+			<div className={`menu-items ${isOpen ? "open" : ""}`}>
+				<Link to="/">Home</Link>
+				<Link to="/Activities">Activiteiten</Link>
+				<Link to="/Timetable">Timetable</Link>
+				<Link to="/Accessibility">Bereikbaarheid</Link>
+				<Link to="/Practical">Praktische info</Link>
+				<Outlet />
+			</div>
+		</div>
+	);
 };
 
 export default HamburgerMenu;

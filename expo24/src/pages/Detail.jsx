1faffef73051ad8b449d20213e2c7d7d8ad6e558
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2024-06-13 15:08:19 +0200

    Andere activiteteiten on home works

--- expo24/src/pages/Detail.jsx
+++ expo24/src/pages/Detail.jsx
@@ -3,6 +3,13 @@ import { useParams, Link } from "react-router-dom";
 import detailCss from "../styles/Detail.module.css";
 import foto from "../assets/vrGames.jpg";
 import activities from "../../activities.json"; // Importeer de JSON-gegevens
+import Activity from "../components/Activity"; // Importeer de Activity component
+
+function getRandomActivities(currentTitle, num) {
+	const filteredActivities = activities.filter((activity) => activity.title !== currentTitle);
+	const shuffled = filteredActivities.sort(() => 0.5 - Math.random());
+	return shuffled.slice(0, num);
+}
 
 function Detail() {
 	const { title } = useParams();
@@ -11,6 +18,8 @@ function Detail() {
 	if (!activity) {
 		return <p>Activiteit niet gevonden</p>;
 	}
+
+	const randomActivities = getRandomActivities(title, 3);
 
 	return (
 		<>
@@ -39,6 +48,15 @@ function Detail() {
 							</div>
 							<hr />
 						</div>
+					))}
+				</div>
+			</div>
+			<hr />
+			<div className={detailCss.RandomActivity}>
+				<h1>Andere activiteiten:</h1>
+				<div className={detailCss.randomActivities}>
+					{randomActivities.map((activity, index) => (
+						<Activity key={index} activity={activity} />
 					))}
 				</div>
 			</div>

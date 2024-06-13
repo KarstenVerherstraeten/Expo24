01841c908e8f9c26d1fb886f3270f2a2dcf4f305
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-06-13 16:12:08 +0200

    Refactor SlideShow component

--- expo24/src/components/SlideShow.jsx
+++ expo24/src/components/SlideShow.jsx
@@ -1,51 +1,34 @@
-import React, { useRef } from "react";
+import React from "react";
 import slideCSS from "../styles/SlideShow.module.css";
 import Voorbeeld from "../assets/react.svg";
 import { Link } from "react-router-dom";
 
 const Slideshow = () => {
-	const scrollRef = useRef(null);
-
-	const handleScroll = (event) => {
-		const container = scrollRef.current;
-		if (event.deltaY !== 0) {
-			event.preventDefault();
-			container.scrollTo({
-				left: container.scrollLeft + event.deltaY,
-				behavior: "smooth",
-			});
-		}
-	};
-
-	return (
-		<div
-			className={slideCSS.scrollContainer}
-			ref={scrollRef}
-			onWheel={handleScroll}
-		>
-			<div className={slideCSS.tile}>
-				<Link to="/Activities">
-					<div className={slideCSS.imageContainer}>
-						<img
-							src={Voorbeeld}
-							alt="Project Background"
-							className={slideCSS.backgroundImage}
-						/>
-						<div className={slideCSS.glassPanel}>
-							{Array.from({ length: 8 }).map((_, index) => (
-								<div
-									className={slideCSS.glass}
-									key={index}
-									style={{ animationDelay: `${index * 0.1}s` }}
-								></div>
-							))}
-						</div>
-					</div>
-					<div className={slideCSS.infoBox}>
-						<p className={slideCSS.infoText}>WORKSHOPS</p>
-					</div>
-				</Link>
-			</div>
+  return (
+    <div className={slideCSS.scrollContainer}>
+      <div className={slideCSS.tile}>
+        <Link to="/Activities">
+          <div className={slideCSS.imageContainer}>
+            <img
+              src={Voorbeeld}
+              alt="Project Background"
+              className={slideCSS.backgroundImage}
+            />
+            <div className={slideCSS.glassPanel}>
+              {Array.from({ length: 8 }).map((_, index) => (
+                <div
+                  className={slideCSS.glass}
+                  key={index}
+                  style={{ animationDelay: `${index * 0.1}s` }}
+                ></div>
+              ))}
+            </div>
+          </div>
+          <div className={slideCSS.infoBox}>
+            <p className={slideCSS.infoText}>WORKSHOPS</p>
+          </div>
+        </Link>
+      </div>
 
       <div className={slideCSS.tile}>
         <div className={slideCSS.imageContainer}>

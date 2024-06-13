89d49c04d38327eb55af42fc778244b7cd4ecd3a
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-06-13 14:05:18 +0200

    Update Footer component and styles

--- expo24/src/components/Footer.jsx
+++ expo24/src/components/Footer.jsx
@@ -11,12 +11,13 @@ function Footer() {
 	return (
 		<>
 			<nav className={css.FooterContainer}>
-				<Link to="/">
-					<img src={LogoW} className={css.whiteLogo} alt="Logo" />
-				</Link>
-				<div>
+				<div className={css.topRow}>
 					<ul className={css.links}>
-						<li>
+					<Link to="/">
+						<img src={LogoW} className={css.whiteLogo} alt="Logo" />
+					</Link>
+					<div className={css.urlElements}>
+					<li>
 							<Link to="/">Home</Link>
 						</li>
 						<li>
@@ -37,24 +38,38 @@ function Footer() {
 						<li>
 							<Link to="/">Projecten</Link>
 						</li> */}
+					</div>
+						
 					</ul>
 					<div className={css.ehbLogo}>
 						<div className={css.socials}>
-							<a target="_blank" href="https://www.linkedin.com/school/erasmushogeschool-brussel/?originalSubdomain=be">
+							<a
+								target="_blank"
+								href="https://www.linkedin.com/school/erasmushogeschool-brussel/?originalSubdomain=be"
+							>
 								<img src={LinkedIn} alt="LinkedIn" />
 							</a>
 
-							<a target="_blank" href="https://www.facebook.com/share/CXqyYvpnAxdgJRcy/">
+							<a
+								target="_blank"
+								href="https://www.facebook.com/share/CXqyYvpnAxdgJRcy/"
+							>
 								<img src={Facebook} alt="Facebook" />
 							</a>
-							<a target="_blank" href="https://www.instagram.com/multimedia.ehb/">
+							<a
+								target="_blank"
+								href="https://www.instagram.com/multimedia.ehb/"
+							>
 								<img src={Instagram} alt="Instagram" />
 							</a>
 							<a target="_blank" href="https://www.tiktok.com/@multimedia.ehb">
 								<img src={Tiktok} alt="Tiktok" />
 							</a>
 						</div>
-						<a target="_blank" 	href="https://www.erasmushogeschool.be/nl/opleidingen/multimedia-creatieve-technologie">
+						<a
+							target="_blank"
+							href="https://www.erasmushogeschool.be/nl/opleidingen/multimedia-creatieve-technologie"
+						>
 							<img className={css.logoEhb} src={LogoEHB} alt="EHB"></img>
 						</a>
 					</div>

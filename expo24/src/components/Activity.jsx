ecc973dbdad8690a3a98e4663312477fa316ed04
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2024-06-13 16:18:41 +0200

    activities width changed + glasmorphisme

--- expo24/src/components/Activity.jsx
+++ expo24/src/components/Activity.jsx
@@ -4,24 +4,26 @@ import ActivityCss from "../styles/Activity.module.css";
 import vrImage from "../assets/vrGames.jpg";
 
 function Activity({ activity }) {
-  return (
-    <div className={ActivityCss.tile}>
-      <Link to={`/activity/${activity.title}`} className={ActivityCss.activityContext}>
-        <div className={ActivityCss.ImageContainer}>
-          <h3 className={ActivityCss.text}>{activity.locatie}</h3>
-          <img src={vrImage} alt="Foto Activiteit" className={ActivityCss.image} />
-          <div className={ActivityCss.glassPanel}>
-            {Array.from({ length: 8 }).map((_, index) => (
-              <div className={ActivityCss.glass} key={index} style={{ animationDelay: `${index * 0.1}s` }}></div>
-            ))}
-          </div>
-        </div>
-        <div className={ActivityCss.info}>
-          <p>{activity.title}</p>
-        </div>
-      </Link>
-    </div>
-  );
+	return (
+		<div className={ActivityCss.tile}>
+			<Link to={`/activity/${activity.title}`} className={ActivityCss.activityContext}>
+				<div className={ActivityCss.ImageContainer}>
+					<h3 className={ActivityCss.text} style={{ backgroundColor: activity.backgroundColor }}>
+						{activity.locatie}
+					</h3>
+					<img src={vrImage} alt="Foto Activiteit" className={ActivityCss.image} />
+					<div className={ActivityCss.glassPanel}>
+						{Array.from({ length: 8 }).map((_, index) => (
+							<div className={ActivityCss.glass} key={index} style={{ animationDelay: `${index * 0.1}s` }}></div>
+						))}
+					</div>
+				</div>
+				<div className={ActivityCss.info}>
+					<p>{activity.title}</p>
+				</div>
+			</Link>
+		</div>
+	);
 }
 
 export default Activity;

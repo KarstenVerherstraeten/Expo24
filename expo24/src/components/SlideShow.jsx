ec6485eb37c2536ee49d179c988679e9364dd4c2
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-06-13 16:05:28 +0200

    Refactor SlideShow component and styles

--- expo24/src/components/SlideShow.jsx
+++ expo24/src/components/SlideShow.jsx
@@ -1,55 +1,180 @@
-import React, { useRef } from "react";
+import React from "react";
 import slideCSS from "../styles/SlideShow.module.css";
 import Voorbeeld from "../assets/react.svg";
 import { Link } from "react-router-dom";
 
 const Slideshow = () => {
-	const scrollRef = useRef(null);
+  return (
+    <div className={slideCSS.scrollContainer}>
+      <div className={slideCSS.tile}>
+        <Link to="/Activities">
+          <div className={slideCSS.imageContainer}>
+            <img
+              src={Voorbeeld}
+              alt="Project Background"
+              className={slideCSS.backgroundImage}
+            />
+            <div className={slideCSS.glassPanel}>
+              {Array.from({ length: 8 }).map((_, index) => (
+                <div
+                  className={slideCSS.glass}
+                  key={index}
+                  style={{ animationDelay: `${index * 0.1}s` }}
+                ></div>
+              ))}
+            </div>
+          </div>
+          <div className={slideCSS.infoBox}>
+            <p className={slideCSS.infoText}>WORKSHOPS</p>
+          </div>
+        </Link>
+      </div>
 
-	const handleScroll = (event) => {
-		const container = scrollRef.current;
-		if (event.deltaY !== 0) {
-			event.preventDefault();
-			container.scrollTo({
-				left: container.scrollLeft + event.deltaY,
-				behavior: "smooth",
-			});
-		}
-	};
+      <div className={slideCSS.tile}>
+        <div className={slideCSS.imageContainer}>
+          <img
+            src={Voorbeeld}
+            alt="Project Background"
+            className={slideCSS.backgroundImage}
+          />
+          <div className={slideCSS.glassPanel}>
+            {Array.from({ length: 8 }).map((_, index) => (
+              <div
+                className={slideCSS.glass}
+                key={index}
+                style={{ animationDelay: `${index * 0.1}s` }}
+              ></div>
+            ))}
+          </div>
+        </div>
+        <div className={slideCSS.infoBox}>
+          <p className={slideCSS.infoText}>ETEN EN DRINKEN</p>
+        </div>
+      </div>
 
-	return (
-		<div
-			className={slideCSS.scrollContainer}
-			ref={scrollRef}
-			onWheel={handleScroll}
-		>
-			<div className={slideCSS.tile}>
-				<Link to="/Activities">
-					<div className={slideCSS.imageContainer}>
-						<img
-							src={Voorbeeld}
-							alt="Project Background"
-							className={slideCSS.backgroundImage}
-						/>
-						<div className={slideCSS.glassPanel}>
-							{Array.from({ length: 8 }).map((_, index) => (
-								<div
-									className={slideCSS.glass}
-									key={index}
-									style={{ animationDelay: `${index * 0.1}s` }}
-								></div>
-							))}
-						</div>
-					</div>
-					<div className={slideCSS.infoBox}>
-						<p className={slideCSS.infoText}>WORKSHOPS</p>
-					</div>
-				</Link>
-			</div>
+      <div className={slideCSS.tile}>
+        <a
+          target="_blank"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSfzePgKhlMeJRjyRnlr-gOlAmdLpUHmD7mprBsdGJmGeBcg4Q/viewform"
+        >
+          <div className={slideCSS.imageContainer}>
+            <img
+              src={Voorbeeld}
+              alt="Project Background"
+              className={slideCSS.backgroundImage}
+            />
+            <div className={slideCSS.glassPanel}>
+              {Array.from({ length: 8 }).map((_, index) => (
+                <div
+                  className={slideCSS.glass}
+                  key={index}
+                  style={{ animationDelay: `${index * 0.1}s` }}
+                ></div>
+              ))}
+            </div>
+          </div>
+          <div className={slideCSS.infoBox}>
+            <p className={slideCSS.infoText}>BARBECUE</p>
+          </div>
+        </a>
+      </div>
 
-			{/* Other tiles... */}
-		</div>
-	);
+      <div className={slideCSS.tile}>
+        <div className={slideCSS.imageContainer}>
+          <img
+            src={Voorbeeld}
+            alt="Project Background"
+            className={slideCSS.backgroundImage}
+          />
+          <div className={slideCSS.glassPanel}>
+            {Array.from({ length: 8 }).map((_, index) => (
+              <div
+                className={slideCSS.glass}
+                key={index}
+                style={{ animationDelay: `${index * 0.1}s` }}
+              ></div>
+            ))}
+          </div>
+        </div>
+        <div className={slideCSS.infoBox}>
+          <p className={slideCSS.infoText}>AWARD SHOW</p>
+        </div>
+      </div>
+
+      <div className={slideCSS.tile}>
+        <Link to="/Activities">
+          <div className={slideCSS.imageContainer}>
+            <img
+              src={Voorbeeld}
+              alt="Project Background"
+              className={slideCSS.backgroundImage}
+            />
+            <div className={slideCSS.glassPanel}>
+              {Array.from({ length: 8 }).map((_, index) => (
+                <div
+                  className={slideCSS.glass}
+                  key={index}
+                  style={{ animationDelay: `${index * 0.1}s` }}
+                ></div>
+              ))}
+            </div>
+          </div>
+          <div className={slideCSS.infoBox}>
+            <p className={slideCSS.infoText}>EINDWERKEN</p>
+          </div>
+        </Link>
+      </div>
+
+      <div className={slideCSS.tile}>
+        <div className={slideCSS.imageContainer}>
+          <img
+            src={Voorbeeld}
+            alt="Project Background"
+            className={slideCSS.backgroundImage}
+          />
+          <div className={slideCSS.glassPanel}>
+            {Array.from({ length: 8 }).map((_, index) => (
+              <div
+                className={slideCSS.glass}
+                key={index}
+                style={{ animationDelay: `${index * 0.1}s` }}
+              ></div>
+            ))}
+          </div>
+        </div>
+        <div className={slideCSS.infoBox}>
+          <p className={slideCSS.infoText}>CONTACTEN LEGGEN</p>
+        </div>
+      </div>
+
+      <div className={slideCSS.tile}>
+        <a
+          target="_blank"
+          href="https://www.erasmushogeschool.be/nl/opleidingen/multimedia-creatieve-technologie"
+        >
+          <div className={slideCSS.imageContainer}>
+            <img
+              src={Voorbeeld}
+              alt="Project Background"
+              className={slideCSS.backgroundImage}
+            />
+            <div className={slideCSS.glassPanel}>
+              {Array.from({ length: 8 }).map((_, index) => (
+                <div
+                  className={slideCSS.glass}
+                  key={index}
+                  style={{ animationDelay: `${index * 0.1}s` }}
+                ></div>
+              ))}
+            </div>
+          </div>
+          <div className={slideCSS.infoBox}>
+            <p className={slideCSS.infoText}>OPLEIDING: MCT</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Slideshow;

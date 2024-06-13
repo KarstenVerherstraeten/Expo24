f330b8e335d7a88ee8e1decd6aef57dbda6553a0
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-06-13 15:50:30 +0200

    Refactor Activity component to improve layout and add animation

--- expo24/src/components/Activity.jsx
+++ expo24/src/components/Activity.jsx
@@ -4,18 +4,24 @@ import ActivityCss from "../styles/Activity.module.css";
 import vrImage from "../assets/vrGames.jpg";
 
 function Activity({ activity }) {
-	return (
-		<Link to={`/activity/${activity.title}`} className={ActivityCss.activityContext}>
-			<div className={ActivityCss.location}>
-				<img src={vrImage} alt="Foto Activiteit" className={ActivityCss.image} />
-				<h3 className={ActivityCss.text}>{activity.locatie}</h3>
-			</div>
-			<div className={ActivityCss.info}>
-				<p>{activity.title}</p>
-				<p>17u</p>
-			</div>
-		</Link>
-	);
+  return (
+    <div className={ActivityCss.tile}>
+      <Link to={`/activity/${activity.title}`} className={ActivityCss.activityContext}>
+        <div className={ActivityCss.ImageContainer}>
+          <h3 className={ActivityCss.text}>{activity.locatie}</h3>
+          <img src={vrImage} alt="Foto Activiteit" className={ActivityCss.image} />
+          <div className={ActivityCss.glassPanel}>
+            {Array.from({ length: 8 }).map((_, index) => (
+              <div className={ActivityCss.glass} key={index} style={{ animationDelay: `${index * 0.1}s` }}></div>
+            ))}
+          </div>
+        </div>
+        <div className={ActivityCss.info}>
+          <p>{activity.title}</p>
+        </div>
+      </Link>
+    </div>
+  );
 }
 
 export default Activity;

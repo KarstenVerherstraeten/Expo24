0d343685b8806014dbf6c47eb0968efd5bdb2273
Youssef <youssef.auassar@student.ehb.be>
2024-06-18 15:02:16 +0200

    Add scroll event listener for button visibility

--- expo24/src/components/VoteNow.jsx
+++ expo24/src/components/VoteNow.jsx
@@ -2,6 +2,18 @@ import React, { useState, useEffect } from "react";
 import styles from "../styles/VoteNow.module.css"; // Import the CSS module
 
 const VoteNow = () => {
+	const [showButton, setShowButton] = useState(false);
+
+	const handleScroll = () => {
+		if (window.scrollY > 50 && window.innerWidth <= 768) {
+			setShowButton(true);
+		} else {
+			setShowButton(false);
+		}
+	};
+
+
+
 	return (
 		<a
 			className={`${styles.voteButton} ${showButton ? styles.show : ""}`}

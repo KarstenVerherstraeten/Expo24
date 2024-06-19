8dcf414de1d370c04fc8d2be4061be1ffdab7319
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-06-19 15:18:53 +0200

    Update function to handle edge cases

--- expo24/src/components/SlideShow.jsx
+++ expo24/src/components/SlideShow.jsx
@@ -63,16 +63,27 @@ const Slideshow = () => {
 						<a href="#">
 							<Link to="/Activities">
 								<div className={slideCSS.imageContainer}>
-									<img src={Workshops} alt="Project Background" className={slideCSS.backgroundImage} />
+									<img
+										src={Workshops}
+										alt="Project Background"
+										className={slideCSS.backgroundImage}
+									/>
 									<div className={slideCSS.glassPanel}>
 										{Array.from({ length: 8 }).map((_, index) => (
-											<div className={slideCSS.glass} key={index} style={{ animationDelay: `${index * 0.1}s` }}></div>
+											<div
+												className={slideCSS.glass}
+												key={index}
+												style={{ animationDelay: `${index * 0.1}s` }}
+											></div>
 										))}
 									</div>
 								</div>
 								<p className={slideCSS.mainText}>WORKSHOPS</p>
 								<div className={slideCSS.infoBox}>
-									<p className={slideCSS.infoText}>Er worden verschillende workshops gegeven waaraan je kunt deelnemen zonder je in te schrijven.</p>
+									<p className={slideCSS.infoText}>
+										Er worden verschillende workshops gegeven waaraan je kunt
+										deelnemen zonder je in te schrijven.
+									</p>
 								</div>
 							</Link>
 						</a>
@@ -81,18 +92,29 @@ const Slideshow = () => {
 
 				<SplideSlide>
 					<div className={slideCSS.tile}>
-						<a href="#">
+						<a href="/activity/DRANKJE%20&%20MUZIEK">
 							<div className={slideCSS.imageContainer}>
-								<img src={Drank} alt="Project Background" className={slideCSS.backgroundImage} />
+								<img
+									src={Drank}
+									alt="Project Background"
+									className={slideCSS.backgroundImage}
+								/>
 								<div className={slideCSS.glassPanel}>
 									{Array.from({ length: 8 }).map((_, index) => (
-										<div className={slideCSS.glass} key={index} style={{ animationDelay: `${index * 0.1}s` }}></div>
+										<div
+											className={slideCSS.glass}
+											key={index}
+											style={{ animationDelay: `${index * 0.1}s` }}
+										></div>
 									))}
 								</div>
 							</div>
 							<p className={slideCSS.mainText}>ETEN EN DRINKEN</p>
 							<div className={slideCSS.infoBox}>
-								<p className={slideCSS.infoText}>Een gratis welkomstdrankje word uitgedeeld en er staan snacks beschikbaar.</p>
+								<p className={slideCSS.infoText}>
+									Een gratis welkomstdrankje word uitgedeeld en er staan snacks
+									beschikbaar.
+								</p>
 							</div>
 						</a>
 					</div>
@@ -100,18 +122,35 @@ const Slideshow = () => {
 
 				<SplideSlide>
 					<div className={slideCSS.tile}>
-						<a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfzePgKhlMeJRjyRnlr-gOlAmdLpUHmD7mprBsdGJmGeBcg4Q/viewform">
+						<a
+							target="_blank"
+							href="https://docs.google.com/forms/d/e/1FAIpQLSfzePgKhlMeJRjyRnlr-gOlAmdLpUHmD7mprBsdGJmGeBcg4Q/viewform"
+						>
 							<div className={slideCSS.imageContainer}>
-								<img src={BBQ} alt="Project Background" className={slideCSS.backgroundImage} />
+								<img
+									src={BBQ}
+									alt="Project Background"
+									className={slideCSS.backgroundImage}
+								/>
 								<div className={slideCSS.glassPanel}>
 									{Array.from({ length: 8 }).map((_, index) => (
-										<div className={slideCSS.glass} key={index} style={{ animationDelay: `${index * 0.1}s`, width: `${15}%` }}></div>
+										<div
+											className={slideCSS.glass}
+											key={index}
+											style={{
+												animationDelay: `${index * 0.1}s`,
+												width: `${15}%`,
+											}}
+										></div>
 									))}
 								</div>
 							</div>
 							<p className={slideCSS.mainText}>BARBECUE</p>
 							<div className={slideCSS.infoBox}>
-								<p className={slideCSS.infoText}>Er is een barbecue die door de studentenvereniging Enigma wordt georganiseerd.</p>
+								<p className={slideCSS.infoText}>
+									Er is een barbecue die door de studentenvereniging Enigma
+									wordt georganiseerd.
+								</p>
 							</div>
 						</a>
 					</div>
@@ -121,16 +160,27 @@ const Slideshow = () => {
 					<div className={slideCSS.tile}>
 						<a href="https://multimedia.brussels/Awards">
 							<div className={slideCSS.imageContainer}>
-								<img src={Award} alt="Project Background" className={slideCSS.backgroundImage} />
+								<img
+									src={Award}
+									alt="Project Background"
+									className={slideCSS.backgroundImage}
+								/>
 								<div className={slideCSS.glassPanel}>
 									{Array.from({ length: 8 }).map((_, index) => (
-										<div className={slideCSS.glass} key={index} style={{ animationDelay: `${index * 0.1}s` }}></div>
+										<div
+											className={slideCSS.glass}
+											key={index}
+											style={{ animationDelay: `${index * 0.1}s` }}
+										></div>
 									))}
 								</div>
 							</div>
 							<p className={slideCSS.mainText}>PRIJSUITREIKING</p>
 							<div className={slideCSS.infoBox}>
-								<p className={slideCSS.infoText}>De prijsuitreiking met de werken van de derdejaars vindt plaats om 20u30.</p>
+								<p className={slideCSS.infoText}>
+									De prijsuitreiking met de werken van de derdejaars vindt
+									plaats om 20u30.
+								</p>
 							</div>
 						</a>
 					</div>
@@ -140,16 +190,27 @@ const Slideshow = () => {
 					<div className={slideCSS.tile}>
 						<Link to="https://multimedia.brussels/">
 							<div className={slideCSS.imageContainer}>
-								<img src={Eindwerken} alt="Project Background" className={slideCSS.backgroundImage} />
+								<img
+									src={Eindwerken}
+									alt="Project Background"
+									className={slideCSS.backgroundImage}
+								/>
 								<div className={slideCSS.glassPanel}>
 									{Array.from({ length: 8 }).map((_, index) => (
-										<div className={slideCSS.glass} key={index} style={{ animationDelay: `${index * 0.1}s` }}></div>
+										<div
+											className={slideCSS.glass}
+											key={index}
+											style={{ animationDelay: `${index * 0.1}s` }}
+										></div>
 									))}
 								</div>
 							</div>
 							<p className={slideCSS.mainText}>EINDWERKEN</p>
 							<div className={slideCSS.infoBox}>
-								<p className={slideCSS.infoText}>Eindwerken van de derdejaars worden getoond waarop je ook kunt stemmen tijdens de expo.</p>
+								<p className={slideCSS.infoText}>
+									Eindwerken van de derdejaars worden getoond waarop je ook kunt
+									stemmen tijdens de expo.
+								</p>
 							</div>
 						</Link>
 					</div>
@@ -157,37 +218,61 @@ const Slideshow = () => {
 
 				<SplideSlide>
 					<div className={slideCSS.tile}>
-						<a href="#">
-							<div className={slideCSS.imageContainer}>
-								<img src={ContactLeggen} alt="Project Background" className={slideCSS.backgroundImage} />
-								<div className={slideCSS.glassPanel}>
-									{Array.from({ length: 8 }).map((_, index) => (
-										<div className={slideCSS.glass} key={index} style={{ animationDelay: `${index * 0.1}s` }}></div>
-									))}
-								</div>
+						<div className={slideCSS.imageContainer}>
+							<img
+								src={ContactLeggen}
+								alt="Project Background"
+								className={slideCSS.backgroundImage}
+							/>
+							<div className={slideCSS.glassPanel}>
+								{Array.from({ length: 8 }).map((_, index) => (
+									<div
+										className={slideCSS.glass}
+										key={index}
+										style={{ animationDelay: `${index * 0.1}s` }}
+									></div>
+								))}
 							</div>
-							<p className={slideCSS.mainText}>CONTACTEN LEGGEN</p>
-							<div className={slideCSS.infoBox}>
-								<p className={slideCSS.infoText}>Er komen veel bedrijven en studenten, dus je hebt de kans om waardevolle connecties maken met iedereen.</p>
-							</div>
-						</a>
+						</div>
+						<p className={slideCSS.mainText}>CONTACTEN LEGGEN</p>
+						<div className={slideCSS.infoBox}>
+							<p className={slideCSS.infoText}>
+								Er komen veel bedrijven en studenten, dus je hebt de kans om
+								waardevolle connecties maken met iedereen.
+							</p>
+						</div>
 					</div>
 				</SplideSlide>
 
 				<SplideSlide>
 					<div className={slideCSS.tile}>
-						<a target="_blank" href="https://www.erasmushogeschool.be/nl/opleidingen/multimedia-creatieve-technologie">
+						<a
+							target="_blank"
+							href="https://www.erasmushogeschool.be/nl/opleidingen/multimedia-creatieve-technologie"
+						>
 							<div className={slideCSS.imageContainer}>
-								<img src={MCT} alt="Project Background" className={slideCSS.backgroundImage} />
+								<img
+									src={MCT}
+									alt="Project Background"
+									className={slideCSS.backgroundImage}
+								/>
 								<div className={slideCSS.glassPanel}>
 									{Array.from({ length: 8 }).map((_, index) => (
-										<div className={slideCSS.glass} key={index} style={{ animationDelay: `${index * 0.1}s` }}></div>
+										<div
+											className={slideCSS.glass}
+											key={index}
+											style={{ animationDelay: `${index * 0.1}s` }}
+										></div>
 									))}
 								</div>
 							</div>
 							<p className={slideCSS.mainText}>OPLEIDING: MCT</p>
 							<div className={slideCSS.infoBox}>
-								<p className={slideCSS.infoText}>De expo wordt georganiseerd door de tweedejaarsstudenten van Multimedia & Creatieve Technologie. Klik hier om een kijkje te nemen op de website van EHB.</p>
+								<p className={slideCSS.infoText}>
+									De expo wordt georganiseerd door de tweedejaarsstudenten van
+									Multimedia & Creatieve Technologie. Klik hier om een kijkje te
+									nemen op de website van EHB.
+								</p>
 							</div>
 						</a>
 					</div>

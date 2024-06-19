0064e8f81201fad9cdfa8fd119efe72c02293f8b
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-06-19 11:53:25 +0200

    Add event end date check and render different component based on event status

--- expo24/src/App.jsx
+++ expo24/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ReactGA from 'react-ga4';
@@ -15,25 +15,41 @@ import Detail from "./pages/Detail";
 import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-	ReactGA.initialize('G-CK17HNSZNZ');
-	return (
-		<Router>
-			<ScrollToTop></ScrollToTop>
-			<Routes>
-				<Route path="/" element={<Navigation />}>
-					<Route index element={<Home />} />
-					<Route path="Accessibility" element={<Accessebiltity />} />
-					<Route path="Activities" element={<Activities />} />
-					<Route path="Timetable" element={<Timetable />} />
-					<Route path="Inschrijving" element={<Inschrijving />} />
-					<Route path="Confirmation" element={<Confirmation />} />
-					<Route path="Faq" element={<Faq />} />
-					<Route path="*" element={<PageNotFound />} />
-					<Route path="activity/:title" element={<Detail />} />
-				</Route>
-			</Routes>
-		</Router>
-	);
+    const eventEndDate = new Date("Jun 28, 2024 12:00:00").getTime(); // Replace with your event end date
+
+    const [isEventOngoing, setIsEventOngoing] = useState(true);
+
+    useEffect(() => {
+        const currentDate = new Date();
+        if (currentDate > eventEndDate) {
+            setIsEventOngoing(false);
+        }
+    }, []);
+
+    ReactGA.initialize('G-CK17HNSZNZ');
+
+    return (
+        <Router>
+            <ScrollToTop />
+            <Routes>
+                {isEventOngoing ? (
+                    <Route path="/" element={<Navigation />}>
+                        <Route index element={<Home />} />
+                        <Route path="Accessibility" element={<Accessebiltity />} />
+                        <Route path="Activities" element={<Activities />} />
+                        <Route path="Timetable" element={<Timetable />} />
+                        <Route path="Inschrijving" element={<Inschrijving />} />
+                        <Route path="Confirmation" element={<Confirmation />} />
+                        <Route path="Faq" element={<Faq />} />
+                        <Route path="*" element={<PageNotFound />} />
+                        <Route path="activity/:title" element={<Detail />} />
+                    </Route>
+                ) : (
+                    <Route path="/" element={<AfterPage />} />
+                )}
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
